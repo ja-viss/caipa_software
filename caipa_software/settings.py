e2be3aec3fff35bcf69f89f3528302782138594f
ja-viss <javier.sayago000@gmail.com>
2025-03-10 20:14:23 -0400

    Update settings.py

--- caipa_software/settings.py
+++ caipa_software/settings.py
@@ -125,10 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_general"),
-]
+STATIC_URL = ''
+
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
